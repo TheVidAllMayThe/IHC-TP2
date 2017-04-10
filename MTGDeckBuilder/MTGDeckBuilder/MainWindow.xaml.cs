a97cc9c15a147e476818955527437708d8fd8b8a
TheVidAllMayThe <davidmoreiraalmeida@hotmail.com>
2017-04-10 15:04:07 +0100

    Main page designed

--- MTGDeckBuilder/MTGDeckBuilder/MainWindow.xaml.cs
+++ MTGDeckBuilder/MTGDeckBuilder/MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace MTGDeckBuilder
         {
             InitializeComponent();
         }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image1.Opacity = 0.5;
+            label.Foreground = Brushes.Black;
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image1.Opacity = 1;
+            label.Foreground = Brushes.White;
+        }
+
+        private void border2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image2.Opacity = 0.5;
+            label_Copy.Foreground = Brushes.Black;
+        }
+
+        private void border2_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image2.Opacity = 1;
+            label_Copy.Foreground = Brushes.White;
+
+        }
+
+        private void border3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image3.Opacity = 0.5;
+            label_Copy1.Foreground = Brushes.Black;
+        }
+
+        private void border3_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image3.Opacity = 1;
+            label_Copy1.Foreground = Brushes.White;
+        }
+
+        private void border4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image4.Opacity = 0.5;
+            label_Copy2.Foreground = Brushes.Black;
+        }
+
+        private void border4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image4.Opacity = 1;
+            label_Copy2.Foreground = Brushes.White;
+        }
     }
 }
