9b2804ce7a5dc7501802ff2c53866439e35c2ef2
TheVidAllMayThe <davidmoreiraalmeida@hotmail.com>
2017-05-19 15:50:06 +0100

    Search decks almost completed. New function to retrieve deck colors made

--- MTGDeckBuilder/MTGDeckBuilder/SearchDecks.xaml.cs
+++ MTGDeckBuilder/MTGDeckBuilder/SearchDecks.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace MTGDeckBuilder
 {
@@ -21,9 +24,67 @@ namespace MTGDeckBuilder
     /// </summary>
     public partial class SearchDecks : Page
     {
+
+        DataTable table;
+        string currentQuerry;
+        int currentPage;
+        Label[] titles;
+        Label[] creators;
+        StackPanel[] colors;
+        StackPanel[] rating;
+
         public SearchDecks()
         {
             InitializeComponent();
+            currentQuerry = "SELECT * FROM Deck";
+            titles = new Label[10];
+            creators = new Label[10];
+            colors = new StackPanel[10];
+            rating = new StackPanel[10];
+            Viewbox viewBox;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Grid deckGrid = new Grid();
+                for (int w = 0; w < 4; w++)
+                    deckGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                titles[i] = new Label();
+                titles[i].Style = Application.Current.Resources["Card Title Style"] as Style;
+                titles[i].Content = "Name of Deck #" + i;
+                viewBox = new Viewbox();
+                viewBox.Child = titles[i];
+                viewBox.HorizontalAlignment = HorizontalAlignment.Left;
+                deckGrid.Children.Add(viewBox);
+                Grid.SetColumn(viewBox, 0);
+
+                creators[i] = new Label();
+                creators[i].Style = Application.Current.Resources["Card Title Style"] as Style;
+                viewBox = new Viewbox();
+                viewBox.Child = creators[i];
+                viewBox.HorizontalAlignment = HorizontalAlignment.Left;
+                deckGrid.Children.Add(viewBox);
+                Grid.SetColumn(viewBox, 1);
+
+                colors[i] = new StackPanel();
+                colors[i].Orientation = Orientation.Horizontal;
+                viewBox = new Viewbox();
+                viewBox.Child = colors[i];
+                deckGrid.Children.Add(viewBox);
+                Grid.SetColumn(viewBox, 2);
+
+                rating[i] = new StackPanel();
+                rating[i].Orientation = Orientation.Horizontal;
+                viewBox = new Viewbox();
+                viewBox.Child = rating[i];
+                deckGrid.Children.Add(viewBox);
+                Grid.SetColumn(viewBox, 2);
+
+                DeckGrid.Children.Add(deckGrid);
+                Grid.SetRow(deckGrid, i);
+
+            }
+            setDecks();
         }
         private void searchBox_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -114,5 +175,124 @@ namespace MTGDeckBuilder
         {
             ((Border)sender).Opacity = 0;
         }
+
+        private void nextPageClick(object sender, RoutedEventArgs e)
+        {
+            setPage(int.Parse(pageTextBox.Text) + 1);
+        }
+
+        private void previousPageClick(object sender, RoutedEventArgs e)
+        {
+            setPage(int.Parse(pageTextBox.Text) - 1);
+        }
+
+        private void pageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (int.Parse(((TextBox)sender).Text) == 0)
+                {
+                    setPage(1);
+                    e.Handled = true;
+                    return;
+                }
+
+                if (int.Parse(((TextBox)sender).Text) > int.Parse(maxPage.Content.ToString().Substring(1)))
+                {
+                    MessageBoxResult result = MessageBox.Show("You have selected an invalid page", "Wrong Page", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    e.Handled = true;
+                    return;
+                }
+
+                setPage(int.Parse(pageTextBox.Text));
+                e.Handled = true;
+            }
+        }
+
+        private void setDecks()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["magicConnect"].ConnectionString;
+
+            SqlConnection thisConnection = new SqlConnection(@cs);
+            thisConnection.Open();
+
+
+            SqlCommand selectCard = new SqlCommand(currentQuerry, thisConnection);
+
+            table = new DataTable("decks");
+            SqlDataAdapter adapt = new SqlDataAdapter(selectCard);
+            adapt.Fill(table);
+
+            setPage(1);
+        }
+
+        private void setPage(int page)
+        {
+            
+            int maxPageInt = table.Rows.Count / 10 + (table.Rows.Count % 10 == 0 ? 0 : 1);
+            currentPage = page;
+            Console.WriteLine(currentQuerry);
+
+            for (int i = 0; i<10; i++)
+            {
+                titles[i % 10].Content = null;
+                creators[i % 10].Content = null;
+                colors[i].Children.Clear();
+            }
+
+            if (page == maxPageInt)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    titles[i].Content = "";
+                    creators[i].Content = "";
+                }
+            }
+
+            if (table.Rows.Count != 0)
+            {
+                for (int i = page * 10 - 10; i < (page == maxPageInt ? table.Rows.Count : page * 10); i++)
+                {
+                    titles[i % 10].Content = table.Rows[i]["name"];
+                    creators[i % 10].Content = table.Rows[i]["creator"];
+
+
+                    string cs = ConfigurationManager.ConnectionStrings["magicConnect"].ConnectionString;
+                    SqlConnection thisConnection = new SqlConnection(@cs);
+                    thisConnection.Open();
+                    string querry = "SELECT * FROM getColorsFromDeck(" + table.Rows[i]["id"] + ")";
+                    SqlCommand powerselect = new SqlCommand(querry, thisConnection);
+                    SqlDataReader querryCommandReader = powerselect.ExecuteReader();
+
+                    while (querryCommandReader.Read())
+                    {
+                        BitmapImage image = new BitmapImage(new Uri("/Mana_"+ querryCommandReader["color"] + ".png", UriKind.Relative));
+                        Image img = new Image();
+                        img.Source = image;
+                        img.VerticalAlignment = VerticalAlignment.Center;
+                        img.HorizontalAlignment = HorizontalAlignment.Center;
+                        img.Height = 200;
+                        img.Width = 200;
+                        img.Margin = new Thickness(25, 50, 25, 50);
+                        colors[i].Children.Add(img);
+                    }
+                }
+            }
+            maxPage.Content = "/" + maxPageInt;
+
+            pageTextBox.Text = "" + page;
+
+            if (page == maxPageInt)
+                nextPage.IsEnabled = false;
+            else
+                nextPage.IsEnabled = true;
+
+            if (page == 1)
+                previousPage.IsEnabled = false;
+            else
+                previousPage.IsEnabled = true;
+            
+        }
+
     }
 }
